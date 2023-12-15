914075c30eebee195df13a1ffa45cb2a3a61bf52
Adryan <adryangeorgem@gmail.com>
2023-12-15 14:07:35 -0400

    ola mundo

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Cliente> clientes = new ArrayList<>();
-        ArrayList<ContaBancaria> contas = new ArrayList<>();
 
 
-        System.out.println("Bem-vindo ao Banco, escolha algumas das opções:\n" + "1- Criar conta\n2- Depositar\n3- Sacar\n4- Verificar saldo\n0- Sair");
         Scanner in = new Scanner(System.in);
         int escolha = in.nextInt();
 
-
-
-        switch(escolha){
-            case 1:
-
-                break;
-
+        while(true){
+            System.out.println("Bem-vindo ao Banco, escolha algumas das opções:\n" + "1- Criar conta\n2- Depositar\n3- Sacar\n4- Verificar saldo\n0- Sair");
         }
     }
 }
